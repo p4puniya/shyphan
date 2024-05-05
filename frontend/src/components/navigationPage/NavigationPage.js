3867021114a57d935d16db103cca825f1751b45d
p4puniya <gauravpny@gmail.com>
2024-05-05 12:34:55 +0530

    todo= render subcomponents of navigation page

--- frontend/src/components/navigationPage/NavigationPage.js
+++ frontend/src/components/navigationPage/NavigationPage.js
@@ -39,17 +39,7 @@ const NavigationPage = () => {
 
   const toggleTheme = () => {
     setDarkMode(!darkMode);
-  }
-  
-  const onClick = ({ key }) => {
-    if (key === '9') { // If Logout option is selected
-      handleLogout();
-    } else {
-      // Handle other menu options if needed
-    }
-  };
-
-  
+  }  
 
   const handleLogout = () => {
     localStorage.removeItem('token'); // Remove token from local storage
